0486aae45628c8f638dca3e8487659e277a4c8a3
olivernyc <oliver@static.network>
2019-07-13 17:33:41 -0400

    Title

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import DocumentTitle from "react-document-title";
 import qs from "qs";
 import AddressInput from "./AddressInput";
 
@@ -26,36 +27,38 @@ export default function Form(props) {
 	};
 
 	return (
-		<form onSubmit={onSubmit}>
-			<h1 className="f4 fw7 mb5 mv3">
-				Check for line of sight to supernodes and hubs
-			</h1>
-			<p className="lh-copy dark-gray">
-				This tool uses the{" "}
-				<a
-					target="_"
-					className="dark-blue no-underline"
-					href="https://www1.nyc.gov/site/doitt/initiatives/3d-building.page"
-				>
-					NYC 3D Building Model
-				</a>{" "}
-				to see if your roof can connect to nearby nodes. The data is
-				from 2014, so results are not 100% accurate.
-			</p>
-			<AddressInput
-				address={address}
-				onChange={onChange}
-				onSelect={onSelect}
-			/>
-			<input
-				tabIndex="2"
-				type="submit"
-				value="Check"
-				disabled={disabled}
-				className={`bn fr pa3 white ${
-					disabled ? "bg-moon-gray" : "bg-red pointer"
-				} br2 fw6 f5-ns f6 ttu shadow mv3 w-auto-ns w-100`}
-			/>
-		</form>
+		<DocumentTitle title="Line of Sight">
+			<form onSubmit={onSubmit}>
+				<h1 className="f4 fw7 mb5 mv3">
+					Check for line of sight to supernodes and hubs
+				</h1>
+				<p className="lh-copy dark-gray">
+					This tool uses the{" "}
+					<a
+						target="_"
+						className="dark-blue no-underline"
+						href="https://www1.nyc.gov/site/doitt/initiatives/3d-building.page"
+					>
+						NYC 3D Building Model
+					</a>{" "}
+					to see if your roof can connect to nearby nodes. The data is
+					from 2014, so results are not 100% accurate.
+				</p>
+				<AddressInput
+					address={address}
+					onChange={onChange}
+					onSelect={onSelect}
+				/>
+				<input
+					tabIndex="2"
+					type="submit"
+					value="Check"
+					disabled={disabled}
+					className={`bn fr pa3 white ${
+						disabled ? "bg-moon-gray" : "bg-red pointer"
+					} br2 fw6 f5-ns f6 ttu shadow mv3 w-auto-ns w-100`}
+				/>
+			</form>
+		</DocumentTitle>
 	);
 }
