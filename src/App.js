e53dd482994998a376efe7e4e570ba417855d1e4
olivernyc <oliver@static.network>
2019-07-13 16:52:00 -0400

    Fixes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Form from "./components/Form";
+import ResultsList from "./components/ResultsList";
 
 export default function App() {
-  return (
-    <div className="helvetica">
-      <Form />
-    </div>
-  );
+	return (
+		<Router>
+			<div className="helvetica pv5-ns pv3 ph3">
+				<div className="measure-wide center">
+					<Route path="/" exact component={Form} />
+					<Route path="/search/" component={ResultsList} />
+				</div>
+			</div>
+		</Router>
+	);
 }
---
+++ src/targets.js
@@ -0,0 +1,86 @@
+export default [
+	{
+		name: "SN1",
+		bin: "1001389"
+	},
+	{
+		name: "7th St.",
+		bin: "1086499"
+	},
+	{
+		name: "Rivington",
+		bin: "1075676"
+	},
+	{
+		name: "Ave D",
+		bin: "1004447"
+	},
+	{
+		name: "1147",
+		bin: "1006456"
+	},
+	{
+		name: "Pierre",
+		bin: "1006327"
+	},
+	{
+		name: "Henry",
+		bin: "1087057"
+	},
+	{
+		name: "1971",
+		bin: "3001646"
+	},
+	{
+		name: "659",
+		bin: "1004052"
+	},
+	{
+		name: "1340",
+		bin: "3039983"
+	},
+	{
+		name: "Ehud",
+		bin: "3050714"
+	},
+	{
+		name: "Flo",
+		bin: "3057781"
+	},
+	{
+		name: "3085",
+		bin: "3073369"
+	},
+	{
+		name: "Dekalb",
+		bin: "3325497"
+	},
+	{
+		name: "RiseBoro",
+		bin: "3387654"
+	},
+	{
+		name: "Soft Surplus",
+		bin: "3319903"
+	},
+	{
+		name: "Simona",
+		bin: "3070130"
+	},
+	{
+		name: "Cypress Inn",
+		bin: "4081837"
+	},
+	{
+		name: "SN3",
+		bin: "3336893"
+	},
+	{
+		name: "SN4",
+		bin: "3029670"
+	},
+	{
+		name: "Guernsey Hub",
+		bin: "3065507"
+	}
+];
