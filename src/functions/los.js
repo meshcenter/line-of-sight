2b1d38d6050291f547845cc18c18a414ca77f324
olivernyc <oliver@static.network>
2019-11-03 22:11:04 -0500

    Add more hubs

--- src/functions/los.js
+++ src/functions/los.js
@@ -1,5 +1,44 @@
 import pg from "pg";
-import targets from "../targets";
+const hubs = [
+	{ id: 3, name: "Brian", bin: 3122755 },
+	{ id: 303, name: "Dekalb", bin: 3325497 },
+	{ id: 329, name: "Simona", bin: 3070130 },
+	{ id: 363, bin: 1012854 },
+	{ id: 407, name: "Henry hub", bin: 1087057 },
+	{ id: 659, bin: 1004045 },
+	{ id: 664, name: "Cypress", bin: 4548430 },
+	{ id: 731, name: "7th St Hub Alphabet City", bin: 1086499 },
+	{ id: 944, name: "Flo", bin: 3057781 },
+	{ id: 1126, bin: 2051270 },
+	{ id: 1147, bin: 1006456 },
+	{ id: 1340, name: "Saratoga", bin: 3039983 },
+	{ id: 1350, name: "Ehud", bin: 3050714 },
+	{ id: 1384, name: "Alex", bin: 1083419 },
+	{ id: 1417, name: "Soft Surplus", bin: 3319903 },
+	{ id: 1515, name: "Domino", bin: 3335796 },
+	{ id: 1567, name: "Pierre", bin: 1006327 },
+	{ id: 1896, bin: 1059774 },
+	{ id: 1971, name: "Two Bridges", bin: 3001646 },
+	{ id: 2090, name: "Guernsey hub", bin: 3065507 },
+	{ id: 2463, name: "Rivington Hotel", bin: 1075676 },
+	{ id: 2646, bin: 3052348 },
+	{ id: 2743, bin: 4082193 },
+	{ id: 2874, bin: 3045578 },
+	{ id: 2932, name: "RiseBoro Youth Center", bin: 3387654 },
+	{ id: 2950, name: "Ave D", bin: 1004447 },
+	{ id: 2959, bin: 3045585 },
+	{ id: 3085, bin: 3073369 },
+	{ id: 3244, bin: 3065067 },
+	{ id: 3461, name: "Prospect Heights (PH)", bin: 3029628 },
+	{ id: 3662, bin: 3045876 },
+	{ id: 3716, bin: 3064719 },
+	{ id: 3946, name: "YWCA", bin: 3000753 },
+	{ id: 4402, bin: 3115969 },
+	{ id: 4431, bin: 3045315 },
+	{ id: 4459, bin: 3115843 },
+	{ id: 4507, bin: 3115952 },
+	{ id: 4530, bin: 3116000 }
+];
 
 export async function handler(event, context) {
 	const { queryStringParameters } = event;
@@ -81,12 +120,12 @@ export async function handler(event, context) {
 		const [lat, lng] = point;
 		const radius = 2 * 5280; // Miles
 		const hubsInRange = [];
-		for (var i = 0; i < targets.length; i++) {
-			const target = targets[i];
-			const targetMidpoint = await getBuildingMidpoint(target.bin);
-			const distance = await getDistance(point, targetMidpoint);
+		for (var i = 0; i < hubs.length; i++) {
+			const hub = hubs[i];
+			const hubMidpoint = await getBuildingMidpoint(hub.bin);
+			const distance = await getDistance(point, hubMidpoint);
 			if (distance < radius) {
-				hubsInRange.push({ ...target, distance });
+				hubsInRange.push({ ...hub, distance });
 			}
 		}
 
